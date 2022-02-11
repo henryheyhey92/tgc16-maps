caa498e579f8e4d427215bbecc8ff5669a87172b
kunxin-chor <chorkunxin@yahoo.com>
2022-02-11 03:14:50 +0000

    Added marker clustering example

--- 01-basic-map/script.js
+++ 01-basic-map/script.js
@@ -21,3 +21,34 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
+// Create a marker overlay 
+let singaporeMarker = L.marker([1.29, 103.85]);
+
+// Add the marker to the map
+// map.add(singaporeMarker);
+singaporeMarker.addTo(map);
+
+let clementiMarker = L.marker([1.3162, 103.7649]);
+clementiMarker.addTo(map);
+
+L.marker([1.2494,103.8303]).addTo(map);
+
+// add a pop-up box to the Singapore box
+singaporeMarker.bindPopup(`<h1>Singapore</h1>
+Singapore (/ˈsɪŋ(ɡ)əpɔːr/), officially the Republic of Singapore, is a sovereign island city-state in maritime Southeast Asia. It lies about one degree of latitude (137 kilometres or 85 miles) north of the equator, off the southern tip of the Malay Peninsula, bordering the Straits of Malacca to the west, the Riau Islands (Indonesia) to the south, and the South China Sea to the east. The country's territory is composed of one main island, 63 satellite islands and islets, and one outlying islet, the combined area of which has increased by 25% since the country's independence as a result of extensive land reclamation projects. It has the third greatest population density in the world. With a multicultural population and recognising the need to respect cultural identities, Singapore has four official languages; English, Malay, Mandarin, and Tamil. English is the lingua franca. Multiracialism is enshrined in the constitution and continues to shape national policies in education, housing, and politics.
+`);
+
+// or we can add an event listener to mark
+clementiMarker.addEventListener('click', function(){
+    alert("I can see my house from here!");
+})
+
+// create a circle
+// first arg is the lat,lng of the circle (center point)
+let circle = L.circle([1.3294,103.8021], {
+    color: 'red',
+    fillColor:'orange',
+    fillOpacity:0.5,
+    radius: 500 // metres
+});
+circle.addTo(map);
